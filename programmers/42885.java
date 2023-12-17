7196fe2ff1eddc5ea69c410ec141b7f03906279e
Minyoung Song <53250432+ankisile@users.noreply.github.com>
2023-12-18 02:19:36 +0900

    solve : 구명보트
    https://school.programmers.co.kr/learn/courses/30/lessons/42885

---
+++ programmers/42885.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        int now = 0;
+        Arrays.sort(people);
+        for(int i=people.length - 1;i >= now;i--){
+            if(people[i] + people[now] <= limit){
+                now++;
+            }
+            answer++;
+        }
+        return answer;
+    }
+}
